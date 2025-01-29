45b8881b13a9db96d3693b4fd0184c9c8f1e1af8
Eric Eang <eange@uci.edu>
2025-01-29 12:16:21 -0800

    Add Jiji

--- src/parts/Menu.jsx
+++ src/parts/Menu.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { useStory } from "../contexts/useStory";
 
 export default function Menu() {
+  // @ts-ignore
   const { setStoryState } = useStory();
   return (
-    <div className="flex flex-col text-center gap-4 justify-center items-center h-dvh text-slate-950">
+    <div className="flex flex-col text-center justify-center items-center text-slate-950 h-dvh">
       <p className="text-3xl/16 font-bold">Jiji's Adventure</p>
       <button
         onClick={() => setStoryState((prev) => (prev += 1))}
-        className="bg-slate-400 p-4 rounded-2xl border-4 text-xl"
+        className="bg-slate-400 p-4 m-4 rounded-2xl border-4 text-xl"
       >
         Click to start!
       </button>
--- src/parts/PartOne.jsx
+++ src/parts/PartOne.jsx
@@ -3,10 +3,15 @@ import { TextBox } from "../components/TextBox";
 
 export default function PartOne() {
   return (
-    <div className="flex items-end min-h-screen text-2xl/8">
-      <TextBox>
-        Jiji needs your help! Someone has stolen her favorite toy fish.
-      </TextBox>
+    <div className="h-dvh flex flex-col justify-end items-center">
+      <div className="flex-grow flex items-center">
+        <img src="/jiji-adventure/jiji.png" className="h-72 animate-sway"></img>
+      </div>
+      <div className="flex items-end text-2xl/8">
+        <TextBox>
+          Jiji needs your help! Someone has stolen her favorite toy fish.
+        </TextBox>
+      </div>
     </div>
   );
 }
