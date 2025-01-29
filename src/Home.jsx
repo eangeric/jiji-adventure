ddbabdf3f5799a82b54c491dbdf422d68d7e8baf
Eric Eang <eange@uci.edu>
2025-01-28 17:55:22 -0800

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center h-dvh text-slate-950">
+        Jiji's Adventure
+        <p>Click to start</p>
+      </div>
+    </>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import "./index.css";
+import Home from "./Home.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
