ea9e200815a50be771bd6916d135995ce0eaa9aa
Eric Eang <eange@uci.edu>
2025-01-28 17:59:07 -0800

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
 import Home from "./Home.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Home />
   </StrictMode>
 );
