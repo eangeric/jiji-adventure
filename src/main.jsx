0603457b1a5beae3f9933dbb6e46bf33b476c3e1
Eric Eang <eange@uci.edu>
2025-01-28 18:45:23 -0800

    Main page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
 import Home from "./Home.jsx";
+import PartOne from "./routes/PartOne.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/jiji-adventure" element={<Home />}></Route>
+        <Route path="/jiji-adventure/1" element={<PartOne />}></Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
---
+++ src/routes/PartOne.jsx
@@ -0,0 +1,8 @@
+export default function PartOne() {
+  return (
+    <div className="flex flex-col justify-center items-center text-center h-dvh">
+      <p>Sorry!</p>
+      <p>Nothing here yet :(</p>
+    </div>
+  );
+}
